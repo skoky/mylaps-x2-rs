dbd05808fdeed09b1af937a95850c2229756f971
Ladislav Skokan (lskokan) <lskokan@cisco.com>
2020-12-27 10:30:10 +0100

    wrapping unsafe

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ mod mylapsx2;
 static mut STOP: bool = false; // FIXME convert to non-static handle
 
 fn main() {
+
     let mut context = 0 as *mut c_void;
     let timeout = time::Duration::from_secs(10);
     let app_name = CString::new("mylpaps-x2-rs").unwrap();
@@ -32,20 +33,16 @@ fn main() {
         Some(_) | None => panic!("MyLaps X2 server name missing"),
     };
 
-    let hostname = CString::new(hostname_param).unwrap();
-
     let sdk_handle = sdk_handle_safe(context, app_name);
 
     setup_notifier(sdk_handle);
 
-    match unsafe { mdp_sdk_appliance_verify(sdk_handle, hostname.as_ptr()) } {
-        false => panic!("Can't create SDK handle"),
-        true => {}
-    }
+    verify_appliance(sdk_handle, hostname_param);
+
     println!("Connecting {}...", hostname_param);
     let now = time::Instant::now();
     while !unsafe { STOP } {
-        unsafe { mdp_sdk_messagequeue_process(sdk_handle, true, 1_000); }
+        wait_for_message(sdk_handle);
         if now.elapsed() >= timeout {
             println!("Timeout waiting for verify");
             exit(1);
@@ -57,7 +54,7 @@ unsafe extern "C" fn notify_verify(handle: mdp_sdk_handle_t,
                                    hostname: *const ::std::os::raw::c_char,
                                    is_verified: bool,
                                    appliance: *const availableappliance_t,
-                                       context: *mut ::std::os::raw::c_void, ) {
+                                   context: *mut ::std::os::raw::c_void, ) {
     let h = CStr::from_ptr(hostname);
     println!("Verification result {} -> {}", h.to_str().unwrap(), is_verified);
     if is_verified {
@@ -67,8 +64,24 @@ unsafe extern "C" fn notify_verify(handle: mdp_sdk_handle_t,
     STOP = true;
 }
 
+fn wait_for_message(sdk_handle: mdp_sdk_handle_t) {
+    unsafe { mdp_sdk_messagequeue_process(sdk_handle, true, 1_000); }
+}
+
 fn setup_notifier(sdk_handle: mdp_sdk_handle_t) {
     unsafe { mdp_sdk_notify_verify_appliance(sdk_handle, Some(notify_verify)); }
+}
+
+fn verify_appliance(sdk_handle: mdp_sdk_handle_t, hostname_param: &str) {
+    match CString::new(hostname_param) {
+        Ok(hostname) => {
+            match unsafe { mdp_sdk_appliance_verify(sdk_handle, hostname.as_ptr()) } {
+                false => panic!("Can't create SDK handle"),
+                true => {}
+            }
+        }
+        Err(e) => panic!("Unable to convert hostname {}", e)
+    }
 }
 
 fn sdk_handle_safe(context: *mut c_void, app_name: CString) -> *mut mdp_sdk_handle_dummystruct {
