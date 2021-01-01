1b90d3a9a1604df0f3784181529eb125d201bebf
Ladislav Skokan (lskokan) <lskokan@cisco.com>
2021-01-01 14:40:51 +0100

    appliance types conversion

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,19 @@ use std::time::Duration;
 
 use clap::{App, Arg};
 
-use crate::mylapsx2::availableappliance_t;
-use crate::mylapsx2::mdp_mac_to_string;
-use crate::mylapsx2::mdp_ipaddress_to_string;
-use crate::mylapsx2::mdp_version_to_string;
 use crate::mylapsx2::availableappliance_is_compatible;
+use crate::mylapsx2::availableappliance_t;
+use crate::mylapsx2::AVAILABLEAPPLIANCETYPE__APPLIANCE_TYPE_X2_PRO_SERVER;
+use crate::mylapsx2::AVAILABLEAPPLIANCETYPE__APPLIANCE_TYPE_X2_SERVER;
+use crate::mylapsx2::mdp_ipaddress_to_string;
+use crate::mylapsx2::mdp_mac_to_string;
 use crate::mylapsx2::mdp_sdk_alloc;
 use crate::mylapsx2::mdp_sdk_appliance_verify;
 use crate::mylapsx2::mdp_sdk_handle_dummystruct;
 use crate::mylapsx2::mdp_sdk_handle_t;
 use crate::mylapsx2::mdp_sdk_messagequeue_process;
 use crate::mylapsx2::mdp_sdk_notify_verify_appliance;
+use crate::mylapsx2::mdp_version_to_string;
 
 mod mylapsx2;
 
@@ -97,13 +99,18 @@ unsafe extern "C" fn notify_verify(_handle: mdp_sdk_handle_t,
 
         let mac = mdp_mac_to_string(buf.as_mut_ptr(), buf.len() as u64, appl.macaddress, true);
         let mac_str = CStr::from_ptr(mac).to_str().unwrap();
-        let build_number = mdp_version_to_string(buf.as_mut_ptr(),buf.len()as u64, appl.buildnumber, true);
+        let build_number = mdp_version_to_string(buf.as_mut_ptr(), buf.len() as u64, appl.buildnumber, true);
         let build_number_str = CStr::from_ptr(build_number).to_str().unwrap();
         let release_name_str = CStr::from_ptr(appl.releasename.as_ptr()).to_str().unwrap();
         let system_setup_str = CStr::from_ptr(appl.systemsetup.as_ptr()).to_str().unwrap();
         let timezone_str = CStr::from_ptr(appl.timezoneid.as_ptr()).to_str().unwrap();
-        println!("Appliance Mac {}, build {}, release {} system setup {} timezone {}",
-                 mac_str, build_number_str, release_name_str, system_setup_str,timezone_str);
+        let appl_type = match appl.type_ as u32 {
+            AVAILABLEAPPLIANCETYPE__APPLIANCE_TYPE_X2_PRO_SERVER => "X2ProServer",
+            AVAILABLEAPPLIANCETYPE__APPLIANCE_TYPE_X2_SERVER => "X2Server",
+            _ => "Unknown"
+        };
+        println!("{} Mac {}, build {}, release {} system setup {} timezone {} type {}",
+                 appl_type, mac_str, build_number_str, release_name_str, system_setup_str, timezone_str, appl_type);
     }
 
     assert!(!context.is_null(), "Context is null in notify_verify handler");
