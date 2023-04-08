8556580cda16968dfd930a5943885591907f3341
Laco Skokan (lskokan) <lskokan@cisco.com>
2023-04-08 11:09:36 +0200

    build process update

--- compile.sh
+++ compile.sh
@@ -3,8 +3,12 @@
 export LD_LIBRARY_PATH=/opt/prj/sdk-master/lib/linux/x86-64
 export LIBRARY_PATH=/opt/prj/sdk-master/lib/linux/x86-64
 export PATH=$HOME/.cargo/bin:$PATH
+
 rustc --version
+cargo update
+
+export RUSTFLAGS=-Awarnings
+cargo clean
 
 mkdir -p src/mylapsx2
-export RUSTFLAGS=-Awarnings
 cargo build
